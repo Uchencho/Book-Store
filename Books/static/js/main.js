7265e0f48cb01fd538fbee541a4e85647480b222
BlessingTolani <65096374+Blessing-Tolani@users.noreply.github.com>
2020-05-26 12:11:53 +0100

    I made the page more interactive

--- Books/static/js/main.js
+++ Books/static/js/main.js
@@ -1,4 +1,38 @@
-function kris(){
-  document.getElementById("thirddiv").style.display="none";
-  document.getElementById("secondiv").style.display="block";
+function showSignup (){
+  $('#thirddiv').hide();
+  $('#secondiv').show();
 }
+
+function hideSignup (){
+  $('#thirddiv').show();
+  $('#secondiv').hide();
+}
+
+function validate (e) {
+  e.preventDefault();
+  var pass = $("#password").val();
+  var confpass = $("#password2").val();
+
+  if (pass !== confpass) {
+    alert("Password don't match: Kindly re-enter the Confirm Password");
+    return false;
+  }
+  
+  $("#login-form").append("<div id='countdown'>Account successfully created, redirecting to Login page in <b id='redirect-count'>5</b></div>");
+  var count = 5;
+  var timer = setInterval(function () {
+      count--;
+      $('#redirect-count').html(count);
+  }, 1000);
+
+  setTimeout(function () {
+      clearInterval(timer);
+      hideSignup();
+      $('#countdown').remove();
+  }, 5000);
+}
+
+$(document).ready(function () {
+  $('#signup-btn').on('click', showSignup);
+  $('#signup-form').on('submit', validate);
+});
